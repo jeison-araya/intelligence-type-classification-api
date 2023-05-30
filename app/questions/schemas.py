28c341c086a65a4e883f630fb5741f8d6b6db37a
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-05-29 22:08:59 -0600

    Question Schema modified

--- app/questions/schemas.py
+++ app/questions/schemas.py
@@ -1,12 +1,20 @@
+from typing import List
 from pydantic import BaseModel
 from pydantic import Field
 from app.database.schemas import BaseConfig
 from app.database.schemas import PyObjectId
 
 
+class Weight(BaseModel):
+    intelligence_id: PyObjectId = Field(default_factory=PyObjectId)
+    weight: int
+
+
 class _BaseQuestion(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
-    text: str
+    question: str
+
+    weights: List[Weight]
 
     class Config(BaseConfig):
         pass
